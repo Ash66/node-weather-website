26e5d62d02819c5d3fb7ef226ad9581328618892
Ashutosh Verma <ntfashu@gmail.com>
2020-04-13 20:06:25 +0530

    Added high and low temperatrues

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/75b04647cc9be57d5ac07c48154e2b0e/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/75b04647cc9be57d5ac07c48154e2b0e/' + latitude + ',' + longitude + '?units=auto'
 
     request({ url, json: true }, (error, {body}) => {
         if (error) {
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain')
+            console.log(body.daily.data[0])
+            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. The max temperature today is ' + body.daily.data[0].temperatureHigh+' with a minimum temperature of '+ body.daily.data[0].temperatureLow+' There is a ' + body.currently.precipProbability + '% chance of rain')
         }
     })
 }
